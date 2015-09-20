7bbc267a249a270d7830a611267f091ab16c02ca
andriivasylchenko <vasylchenko.andrii@gmail.com>
2015-09-20 11:17:09 -0400

    lab 2.37 results

---
+++ canteenPOS/platforms/android/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":2053088998,"date":1442761079985,"machine":"ibm-virtual-machine"}
+/* Date: Sun Sep 20 2015 10:57:59 GMT-0400 (EDT) */
---
+++ canteenPOS/www/js/index.js
@@ -0,0 +1,16 @@
+var Messages = {
+    // Add here your messages for the default language.
+    // Generate a similar file with a language suffix containing the translated messages.
+    // key1 : message1,
+};
+
+var wlInitOptions = {
+    // Options to initialize with the WL.Client object.
+    // For initialization options please refer to IBM MobileFirst Platform Foundation Knowledge Center.
+};
+
+// Called automatically after MFP framework initialization by WL.Client.init(wlInitOptions).
+function wlCommonInit(){
+	// Common initialization code goes here
+    WL.Client.connect();
+}
